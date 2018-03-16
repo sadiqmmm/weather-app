d440286496387d8bfde122f723da3fa73038eea2
Mohammed Sadiq <sadiqmmm@gmail.com>
2018-03-16 22:35:46 +0530

    Fix the css

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 import { WeatherProvider } from '../../providers/weather/weather';
 import { Storage } from '@ionic/storage';
+
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
--- src/providers/weather/weather.ts
+++ src/providers/weather/weather.ts
@@ -1,13 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
-
-/*
-  Generated class for the WeatherProvider provider.
-
-  See https://angular.io/guide/dependency-injection for more info on providers
-  and Angular DI.
-*/
+ 
 @Injectable()
 export class WeatherProvider {
   apiKey = '20ec671713e0e402';
